20f15ea3233ebdfebb363c2d98560e030bba5d63
IstominArseniy <istomin.aiu@phystech.edu>
2025-02-10 16:42:32 +0300

    fix PA behaviour

--- draw_data.py
+++ draw_data.py
@@ -1,7 +1,23 @@
 import numpy as np
 from matplotlib import pyplot as plt
+
+def shift_angle(angle):
+    angle = np.asarray(angle)
+    scalar_in = (angle.ndim==0)
+    angle_vals = np.array(angle, copy=False, ndmin=1, dtype=float)
+    for i in range(angle_vals.shape[0]):
+        while angle_vals[i] > np.pi / 2:
+            angle_vals[i] -= np.pi
+        while angle_vals[i] < -np.pi / 2:
+            angle_vals[i] += np.pi
+    if scalar_in:
+        return angle_vals[0]
+    else:
+        return angle_vals
+
+
 print("print 0 to see initial values and 1 to see calculated values")
-request = input()
+request = int(input())
 if request == 0:
     file_name = "./bin/my_output/my_run_0.dat"
 else:
@@ -10,6 +26,7 @@ phases = []
 Is = []
 Vs = []
 PAs = []
+Fs = []
 with open(file_name) as f:
     lines = f.readlines()
     for line in lines:
@@ -17,7 +34,7 @@ with open(file_name) as f:
         if not np.isnan(float(tmp[1])) and not np.isnan(float(tmp[2])) and not np.isnan(float(tmp[3])):
             Is.append(float(tmp[1]))
             Vs.append(float(tmp[2]))
-            PAs.append(float(tmp[3]))
+            PAs.append(shift_angle(float(tmp[3]) * np.pi / 180) * 180 / np.pi)
             phases.append(float(tmp[0]))
 f1 = plt.figure()
 f2 = plt.figure()
@@ -27,7 +44,7 @@ ax1.plot(phases, Is, label='I')
 ax1.plot(phases, Vs, label='V')
 ax1.legend()
 f1.savefig('I_V_output.png')
-ax2.plot(phases, PAs, label='PA')
+ax2.scatter(phases, PAs, label='PA')
 ax2.legend()
 f2.savefig('PA_output.png')
 
--- draw_theta.py
+++ draw_theta.py
@@ -1,11 +1,30 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+def shift_angle(angle):
+    angle = np.asarray(angle)
+    scalar_in = (angle.ndim==0)
+    angle_vals = np.array(angle, copy=False, ndmin=1, dtype=float)
+    for i in range(angle_vals.shape[0]):
+        while angle_vals[i] > np.pi / 2:
+            angle_vals[i] -= np.pi
+        while angle_vals[i] < -np.pi / 2:
+            angle_vals[i] += np.pi
+    if scalar_in:
+        return angle_vals[0]
+    else:
+        return angle_vals
+
 xs = []
 th1 = []
 th2 = []
-
-file_name = "./bin/my_output/my_run_theta.dat"
+freq = []
+Fs = []
+print("Print phase")
+phase = float(input())
+phase_str = f"{phase:.6f}"
+file_name = "./bin/my_output/my_run_theta_data/my_run_theta_" + phase_str + ".dat"
 with open(file_name) as f:
     lines = f.readlines()
     for line in lines:
@@ -13,16 +32,31 @@ with open(file_name) as f:
         xs.append(float(tmp[0]))
         th1.append(float(tmp[1]))
         th2.append(float(tmp[2]))
+        freq.append(float(tmp[3]))
+        Fs.append(float(tmp[4]))
+xs = np.array(xs)
+th1 = np.array(th1)
+th2 = np.array(th2)
+freq = np.array(freq)
+Fs = np.array(Fs)
 f1 = plt.figure()
 f2 = plt.figure()
+f3 = plt.figure()
 ax1 = f1.add_subplot(111)
 ax2 = f2.add_subplot(111)
-
-ax1.plot(xs, th1)
+ax3 = f3.add_subplot(111)
+ax1.plot(xs, shift_angle(th1), label=r'$\theta$')
+ax1.plot(xs, shift_angle(Fs), label=r'$\beta + \delta$')
+ax1.plot(xs, shift_angle(np.array(Fs) - np.array(th1)), label=r'$\beta + \delta - \theta$')
+ax1.plot()
+ax1.legend()
 f1.savefig('x_th1.png')
 #ax2.scatter(xs[::10], th2[::10], marker='.')
 ax2.plot(xs, th2)
+#ax2.plot(xs, 0.05 + 0.01 * np.sin(freq * xs))
 f2.savefig('x_th2.png')
+ax3.plot(xs, freq)
+f3.savefig('tests.png')
 
 
 
