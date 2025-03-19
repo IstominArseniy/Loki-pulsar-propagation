fdd5c4d4964c04a1ef5ca8b92c9eaa19cd259b59
IstominArseniy <istomin.aiu@phystech.edu>
2025-03-19 16:40:36 +0300

    delete unnecessary function from function.cpp and .h

--- lib/functions.cpp
+++ lib/functions.cpp
@@ -6,38 +6,6 @@ using namespace std;
 
 using Eigen::Vector3d;
 
-double SCALAR (vector <double> vec1, vector <double> vec2) {
-  return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2];
-} // Scalar product
-vector <double> CROSS (vector <double> vec1, vector <double> vec2) {
-  vector <double> temp(3);
-  temp[0] = -vec1[2] * vec2[1] + vec1[1] * vec2[2];
-  temp[1] = vec1[2] * vec2[0] - vec2[2] * vec1[0];
-  temp[2] = -vec1[1] * vec2[0] + vec2[1] * vec1[0];
-  return temp;
-} // Vector product
-double NORM (vector <double> vec) {
-  return sqrt(SCALAR(vec, vec));
-} // Length of vector
-vector <double> SUM (vector <double> vec1, vector <double> vec2) {
-  vector <double> temp(3);
-  temp[0] = vec1[0] + vec2[0];
-  temp[1] = vec1[1] + vec2[1];
-  temp[2] = vec1[2] + vec2[2];
-  return temp;
-} // Sum
-
-vector <double> TIMES (double a, vector <double> vec) {
-  vector <double> temp(3);
-  temp[0] = vec[0] * a;
-  temp[1] = vec[1] * a;
-  temp[2] = vec[2] * a;
-  return temp;
-} // Multiply vector by double
-vector <double> NORMALIZE (vector <double> vec) {
-  return TIMES(1.0 / NORM(vec), vec);
-} // Unitize given vector
-
 double ANGLE (Vector3d vec1, Vector3d vec2) {
   return 2 * atan2((vec2.normalized() - vec1.normalized()).norm(), (vec2.normalized() + vec1.normalized()).norm());
 } // Angle
--- lib/functions.h
+++ lib/functions.h
@@ -3,12 +3,5 @@
 using namespace std;
 using Eigen::Vector3d;
 
-
-double SCALAR (vector <double> vec1, vector <double> vec2);
-vector <double> CROSS (vector <double> vec1, vector <double> vec2);
-double NORM (vector <double> vec);
-vector <double> SUM (vector <double> vec1, vector <double> vec2);
-vector <double> TIMES (double a, vector <double> vec);
-vector <double> NORMALIZE (vector <double> vec);
 double ANGLE (Vector3d vec1, Vector3d vec2);
 double Arcsinh(double x);
